f56aaa2c357c6e27cc07b98f6966ac8c6c9eb888
nullBlade <stepan.gaidukevich@gmail.com>
2023-07-26 23:00:10 +0300

    reactors part 1

--- src/main/java/org/antarcticgardens/newage/NewAgeBlocks.java
+++ src/main/java/org/antarcticgardens/newage/NewAgeBlocks.java
@@ -2,6 +2,7 @@ package org.antarcticgardens.newage;
 
 import com.simibubi.create.content.kinetics.BlockStressDefaults;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import org.antarcticgardens.newage.content.energiser.EnergiserBlock;
@@ -14,6 +15,7 @@ import org.antarcticgardens.newage.content.heat.heatpipe.HeatPipeBlock;
 import org.antarcticgardens.newage.content.heat.heatpump.HeatPumpBlock;
 import org.antarcticgardens.newage.content.heat.solarheatingplate.SolarHeatingPlateBlock;
 import org.antarcticgardens.newage.content.heat.stirlingengine.StirlingEngineBlock;
+import org.antarcticgardens.newage.content.reactor.ReactorBlock;
 
 import static org.antarcticgardens.newage.CreateNewAge.REGISTRATE;
 
@@ -24,6 +26,7 @@ public class NewAgeBlocks {
 
     public static final BlockEntry<EnergiserBlock> ENERGISER_T1 =
             REGISTRATE.block("energiser_t1", EnergiserBlock::new)
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .properties(BlockBehaviour.Properties::noOcclusion)
                     .transform(BlockStressDefaults.setImpact(2.0))
                     .simpleItem()
@@ -31,6 +34,7 @@ public class NewAgeBlocks {
 
     public static final BlockEntry<CarbonBrushesBlock> CARBON_BRUSHES =
             REGISTRATE.block("carbon_brushes", CarbonBrushesBlock::new)
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .properties(BlockBehaviour.Properties::noOcclusion)
                     .simpleItem()
                     .register();
@@ -38,23 +42,27 @@ public class NewAgeBlocks {
     public static final BlockEntry<HeatPipeBlock> HEAT_PIPE =
             REGISTRATE.block("heat_pipe", HeatPipeBlock::new)
                     .properties(BlockBehaviour.Properties::noOcclusion)
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .simpleItem()
                     .register();
 
     public static final BlockEntry<HeatPumpBlock> HEAT_PUMP =
             REGISTRATE.block("heat_pump", HeatPumpBlock::new)
                     .properties(BlockBehaviour.Properties::noOcclusion)
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .simpleItem()
                     .register();
 
     public static final BlockEntry<HeaterBlock> HEATER =
             REGISTRATE.block("heater", HeaterBlock::new)
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .properties(BlockBehaviour.Properties::noOcclusion)
                     .simpleItem()
                     .register();
 
     public static final BlockEntry<GeneratorCoilBlock> GENERATOR_COIL =
             REGISTRATE.block("generator_coil", GeneratorCoilBlock::new)
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .properties(BlockBehaviour.Properties::noOcclusion)
                     .transform(BlockStressDefaults.setImpact(12.0f))
                     .simpleItem()
@@ -63,6 +71,7 @@ public class NewAgeBlocks {
 
     public static final BlockEntry<StirlingEngineBlock> STIRLING_ENGINE =
             REGISTRATE.block("stirling_engine", properties -> new StirlingEngineBlock(properties, NewAgeBlockEntityTypes.STIRLING_ENGINE))
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .properties(BlockBehaviour.Properties::noOcclusion)
                     .transform(BlockStressDefaults.setCapacity(32.0))
                     .simpleItem()
@@ -70,21 +79,39 @@ public class NewAgeBlocks {
 
     public static final BlockEntry<MagnetiteBlock> MAGNETITE_BLOCK =
             REGISTRATE.block("magnetite_block", MagnetiteBlock::new)
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
+                    .simpleItem()
+                    .register();
+
+    public static final BlockEntry<ReactorBlock> REACTOR_CASING =
+            REGISTRATE.block("reactor_casing", ReactorBlock::new)
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
+                    .simpleItem()
+                    .register();
+
+    public static final BlockEntry<ReactorBlock> REACTOR_GLASS =
+            REGISTRATE.block("reactor_glass", ReactorBlock::new)
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
+                    .properties(BlockBehaviour.Properties::noOcclusion)
+                    .addLayer(() -> RenderType::cutout)
                     .simpleItem()
                     .register();
 
     public static final BlockEntry<Block> BASIC_SOLAR_HEATING_PLATE =
             REGISTRATE.block("basic_solar_heating_plate", SolarHeatingPlateBlock::createBasic)
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .simpleItem()
                     .register();
 
     public static final BlockEntry<Block> ADVANCED_SOLAR_HEATING_PLATE =
             REGISTRATE.block("advanced_solar_heating_plate", SolarHeatingPlateBlock::createAdvanced)
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .simpleItem()
                     .register();
 
     public static final BlockEntry<ElectricalConnectorBlock> ELECTRICAL_CONNECTOR =
             REGISTRATE.block("electrical_connector", ElectricalConnectorBlock::new)
+                    .properties(BlockBehaviour.Properties::requiresCorrectToolForDrops)
                     .properties(BlockBehaviour.Properties::noOcclusion)
                     .simpleItem()
                     .register();
---
+++ src/main/java/org/antarcticgardens/newage/content/reactor/ReactorBlock.java
@@ -0,0 +1,9 @@
+package org.antarcticgardens.newage.content.reactor;
+
+import net.minecraft.world.level.block.Block;
+
+public class ReactorBlock extends Block {
+    public ReactorBlock(Properties properties) {
+        super(properties);
+    }
+}
