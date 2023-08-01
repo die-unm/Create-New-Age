b32fb5b4a31187808e406818a4b18263dcee534f
nullBlade <stepan.gaidukevich@gmail.com>
2023-08-01 04:07:34 +0300

    More heating ponder work

--- src/main/java/org/antarcticgardens/newage/content/heat/HeatingPonder.java
+++ src/main/java/org/antarcticgardens/newage/content/heat/HeatingPonder.java
@@ -5,6 +5,7 @@ import com.simibubi.create.foundation.ponder.SceneBuildingUtil;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.Vec3;
+import org.antarcticgardens.newage.NewAgeBlocks;
 
 public class HeatingPonder {
 
@@ -53,11 +54,31 @@ public class HeatingPonder {
         scene.idle(40);
 
         scene.addKeyframe();
-        scene.idleSeconds(20);
+        scene.idle(15);
 
         scene.world.modifyBlock(util.grid.at(3, 1, 1),
                 blockState -> blockState.setValue(BlockStateProperties.UP, true),
                 false);
+        scene.world.showSection(util.select.position(3, 2, 1), Direction.DOWN);
+        scene.rotateCameraY(35);
+        scene.idleSeconds(1);
+
+        scene.overlay.showText(60).placeNearTarget()
+                .text("").pointAt(new Vec3(3.5, 2.5, 1.5));
+        scene.world.setKineticSpeed(util.select.position(3, 2, 1), 8);
+        scene.effects.rotationSpeedIndicator(util.grid.at(3, 2, 1));
+
+        scene.idleSeconds(4);
+
+        scene.world.setBlock(util.grid.at(1, 1, 1), NewAgeBlocks.ADVANCED_SOLAR_HEATING_PLATE.getDefaultState(), true);
+
+        scene.idle(20);
+
+        scene.overlay.showText(60).placeNearTarget()
+                .text("").pointAt(new Vec3(3.5, 2.5, 1.5));
+        scene.world.setKineticSpeed(util.select.position(3, 2, 1), 16);
+        scene.effects.rotationSpeedIndicator(util.grid.at(3, 2, 1));
+        scene.idleSeconds(4);
 
     }
 
