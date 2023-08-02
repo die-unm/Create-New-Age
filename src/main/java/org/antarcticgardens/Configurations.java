c189ff79b3e53acd29d81f548a9b8a14d2b5b34f
nullBlade <stepan.gaidukevich@gmail.com>
2023-08-02 18:54:43 +0300

    Made compat with creat crafts and additions (they aren't out for 1.20.1 yet)

--- src/main/java/org/antarcticgardens/Configurations.java
+++ src/main/java/org/antarcticgardens/Configurations.java
@@ -4,6 +4,6 @@ public class Configurations {
 
     public static int MAX_RODS_IN_DIRECTION = 32;
 
-    public static float SU_TO_ENERGY = 0.4f;
+    public static float SU_TO_ENERGY = (float) 240 / 8192; // Making compatible with create: crafts and additions
 
 }
