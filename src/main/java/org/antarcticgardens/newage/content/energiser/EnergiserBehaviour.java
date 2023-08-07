98200e669d8e42a05bd64595c1a89170690d23fd
nullBlade <stepan.gaidukevich@gmail.com>
2023-08-07 19:10:54 +0300

    First start pass

--- src/main/java/org/antarcticgardens/newage/content/energiser/EnergiserBehaviour.java
+++ src/main/java/org/antarcticgardens/newage/content/energiser/EnergiserBehaviour.java
@@ -9,6 +9,7 @@ import net.minecraft.nbt.CompoundTag;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.item.ItemStack;
+import org.antarcticgardens.newage.CreateNewAge;
 import org.joml.Math;
 
 import java.util.List;
@@ -30,7 +31,7 @@ public class EnergiserBehaviour extends BeltProcessingBehaviour {
             return null;
         }
         
-        List<EnergisingRecipe> recipes = be.getLevel().getRecipeManager().getAllRecipesFor(EnergisingRecipe.type.getType());
+        List<EnergisingRecipe> recipes = be.getLevel().getRecipeManager().getAllRecipesFor(CreateNewAge.type.getType());
 
         for (EnergisingRecipe recipe : recipes) {
             if (recipe.test(stack)) {
