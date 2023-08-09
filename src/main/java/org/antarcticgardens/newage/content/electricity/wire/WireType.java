87d7b0b6874e9fcbd0bf274b9b8eb043b938ecc7
Kastus Talstoj <kanstancin.talstoj@gmail.com>
2023-08-09 03:42:09 +0300

    Wire rendering

--- src/main/java/org/antarcticgardens/newage/content/electricity/wire/WireType.java
+++ src/main/java/org/antarcticgardens/newage/content/electricity/wire/WireType.java
@@ -1,15 +1,27 @@
 package org.antarcticgardens.newage.content.electricity.wire;
 
 public enum WireType {
-    COPPER(1024);
+    COPPER(1024, new int[] { 158, 88, 75, 255 }, new int[] { 173, 108, 92, 255 });
 
     private final long conductivity;
+    private final int[] color1;
+    private final int[] color2;
 
-    WireType(int conductivity) {
+    WireType(int conductivity, int[] color1, int[] color2) {
         this.conductivity = conductivity;
+        this.color1 = color1;
+        this.color2 = color2;
     }
 
     public long getConductivity() {
         return conductivity;
+    }
+
+    public int[] getColor1() {
+        return color1;
+    }
+
+    public int[] getColor2() {
+        return color2;
     }
 }
