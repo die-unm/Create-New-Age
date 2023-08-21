730da6398dfe80fd9268372531f20d9e4a74eacc
nullBlade <stepan.gaidukevich@gmail.com>
2023-08-21 12:07:00 +0300

    Finished generation ponder

--- src/main/java/org/antarcticgardens/newage/content/generation/GenerationPonder.java
+++ src/main/java/org/antarcticgardens/newage/content/generation/GenerationPonder.java
@@ -1,11 +1,14 @@
 package org.antarcticgardens.newage.content.generation;
 
 import com.simibubi.create.content.kinetics.gauge.StressGaugeBlockEntity;
+import com.simibubi.create.foundation.ponder.PonderPalette;
 import com.simibubi.create.foundation.ponder.SceneBuilder;
 import com.simibubi.create.foundation.ponder.SceneBuildingUtil;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
 import org.antarcticgardens.newage.NewAgeBlocks;
 import org.antarcticgardens.newage.content.electricity.connector.ElectricalConnectorBlockEntity;
 import org.antarcticgardens.newage.content.electricity.wire.WireType;
@@ -75,7 +78,13 @@ public class GenerationPonder {
 
         scene.world.setKineticSpeed(util.select.fromTo(4, 3, 3, 4, 3, 8), 32);
 
-        scene.idle(30);
+        scene.idle(15);
+
+        scene.overlay.showText(100)
+                .pointAt(new Vec3(4.5, 5.5, 5.5))
+                .text("");
+
+        scene.idle(110);
 
         scene.world.showSection(util.select.position(4, 1, 1), Direction.DOWN);
 
@@ -95,7 +104,14 @@ public class GenerationPonder {
 
         scene.world.showSection(util.select.position(4, 2, 1), Direction.DOWN);
 
-        scene.idle(15);
+        scene.idle(10);
+
+        scene.overlay.showText(100)
+                .pointAt(new Vec3(4.5, 3.5, 4.5))
+                .text("");
+        scene.overlay.chaseBoundingBoxOutline(PonderPalette.BLUE, new Object(), new AABB(4, 3, 4, 5, 4, 5), 100);
+
+        scene.idle(110);
 
         scene.world.modifyBlockEntity(new BlockPos(4, 2, 4), ElectricalConnectorBlockEntity.class, en -> {
             en.connect((ElectricalConnectorBlockEntity) en.getLevel().getBlockEntity(new BlockPos(4, 2, 1)), WireType.IRON);
@@ -111,9 +127,25 @@ public class GenerationPonder {
 
         scene.idle(20);
 
-        replaceCoil(scene, util, NewAgeBlocks.REDSTONE_MAGNET.getDefaultState());
+        scene.overlay.showText(140)
+                .pointAt(new Vec3(4.5, 3.5, 4.5))
+                .text("");
 
-        scene.idle(10);
+        scene.idle(150);
+
+        scene.overlay.showText(20)
+                .pointAt(new Vec3(4.5, 3.5, 5.5))
+                .text("");
+
+        scene.idle(20);
+
+        replaceCoil(scene, util, NewAgeBlocks.REDSTONE_MAGNET.getDefaultState());
+        scene.overlay.showText(20)
+                .pointAt(new Vec3(4.5, 3.5, 5.5))
+                .text("");
+
+
+        scene.idle(30);
 
         scene.rotateCameraY(180);
 
@@ -126,6 +158,12 @@ public class GenerationPonder {
 
         scene.idle(20);
 
+        scene.overlay.showText(150)
+                .pointAt(new Vec3(4.5, 3.5, 8.5))
+                .text("");
+
+        scene.idle(160);
+
         replaceCoil(scene, util, NewAgeBlocks.LAYERED_MAGNET.getDefaultState());
 
         scene.world.modifyBlockEntityNBT(util.select.position(4, 3, 8), StressGaugeBlockEntity.class,
@@ -137,6 +175,10 @@ public class GenerationPonder {
 
         scene.world.modifyBlockEntityNBT(util.select.position(4, 3, 8), StressGaugeBlockEntity.class,
                 nbt -> nbt.putFloat("Value", 1.0f));
+
+        scene.idle(20);
+
+        scene.markAsFinished();
 
     }
 
