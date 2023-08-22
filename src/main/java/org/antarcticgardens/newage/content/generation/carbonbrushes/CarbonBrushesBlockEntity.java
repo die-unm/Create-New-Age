b59a8d23f6f78ffcc971de7b081a16dee76418b6
Kastus Talstoj <kanstancin.talstoj@gmail.com>
2023-08-23 00:25:13 +0300

    made carbon brushes energy container resizable

--- src/main/java/org/antarcticgardens/newage/content/generation/carbonbrushes/CarbonBrushesBlockEntity.java
+++ src/main/java/org/antarcticgardens/newage/content/generation/carbonbrushes/CarbonBrushesBlockEntity.java
@@ -5,8 +5,6 @@ import com.simibubi.create.content.kinetics.base.DirectionalKineticBlock;
 import com.simibubi.create.content.kinetics.base.KineticBlockEntity;
 import com.simibubi.create.foundation.utility.Lang;
 import earth.terrarium.botarium.common.energy.base.BotariumEnergyBlock;
-import earth.terrarium.botarium.common.energy.impl.ExtractOnlyEnergyContainer;
-import earth.terrarium.botarium.common.energy.impl.WrappedBlockEnergyContainer;
 import earth.terrarium.botarium.common.energy.util.EnergyHooks;
 import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
@@ -18,19 +16,20 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.antarcticgardens.newage.config.NewAgeConfig;
 import org.antarcticgardens.newage.content.generation.generatorcoil.GeneratorCoilBlock;
 import org.antarcticgardens.newage.content.generation.generatorcoil.GeneratorCoilBlockEntity;
+import org.antarcticgardens.newage.energy.ExtractOnlyResizableEnergyContainer;
 import org.antarcticgardens.newage.tools.StringFormattingTool;
 
 import java.util.List;
 
-public class CarbonBrushesBlockEntity extends KineticBlockEntity implements BotariumEnergyBlock<WrappedBlockEnergyContainer>, IHaveGoggleInformation {
-    private final WrappedBlockEnergyContainer energyContainer;
+public class CarbonBrushesBlockEntity extends KineticBlockEntity implements BotariumEnergyBlock<ExtractOnlyResizableEnergyContainer>, IHaveGoggleInformation {
+    private final ExtractOnlyResizableEnergyContainer energyContainer;
 
     private int lastOutput = 0;
 
     public CarbonBrushesBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
 
-        energyContainer = new WrappedBlockEnergyContainer(this, new ExtractOnlyEnergyContainer(25000));
+        energyContainer = new ExtractOnlyResizableEnergyContainer(0);
 
         setLazyTickRate(20);
     }
@@ -52,14 +51,6 @@ public class CarbonBrushesBlockEntity extends KineticBlockEntity implements Bota
         Lang.translate("tooltip.create_new_age.energy_stats")
                 .style(ChatFormatting.WHITE).forGoggles(tooltip);
 
-        Lang.translate("tooltip.create_new_age.energy_stored")
-                .style(ChatFormatting.GRAY)
-                .forGoggles(tooltip);
-
-        Lang.translate("tooltip.create_new_age.energy_storage", StringFormattingTool.formatLong(energyContainer.getStoredEnergy()), StringFormattingTool.formatLong(energyContainer.getMaxCapacity()))
-                .style(ChatFormatting.AQUA)
-                .forGoggles(tooltip, 1);
-
         Lang.translate("tooltip.create_new_age.energy_output")
                 .style(ChatFormatting.GRAY)
                 .forGoggles(tooltip);
@@ -78,6 +69,8 @@ public class CarbonBrushesBlockEntity extends KineticBlockEntity implements Bota
         super.tick();
         if (level == null || level.isClientSide) return;
         Direction facing = getBlockState().getValue(DirectionalKineticBlock.FACING);
+
+        energyContainer.setMaxCapacity(lastOutput * 20L);
 
         int coilsLeft = NewAgeConfig.getCommon().maxCoils.get();
         lastOutput = 0;
@@ -114,7 +107,7 @@ public class CarbonBrushesBlockEntity extends KineticBlockEntity implements Bota
     }
 
     @Override
-    public WrappedBlockEnergyContainer getEnergyStorage() {
+    public ExtractOnlyResizableEnergyContainer getEnergyStorage() {
         return energyContainer;
     }
 }
