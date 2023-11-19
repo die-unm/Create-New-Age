f81cea90252a032c1488419aa30a4cfa34f44679
Kastus Talstoj <kanstancin.talstoj@gmail.com>
2023-11-19 10:31:28 +0300

    oof.

--- src/main/java/org/antarcticgardens/newage/energy/EnergyHooks.java
+++ src/main/java/org/antarcticgardens/newage/energy/EnergyHooks.java
@@ -8,7 +8,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
-import static earth.terrarium.botarium.common.energy.util.EnergyHooks.safeGetBlockEnergyManager;
+import static earth.terrarium.botarium.api.energy.EnergyHooks.safeGetBlockEnergyManager;
 
 public class EnergyHooks {
     public static <T extends BlockEntity & EnergyBlock> void distributeEnergyNearby(T energyBlock, long amount) {
@@ -21,14 +21,9 @@ public class EnergyHooks {
                 .map(pair -> Pair.of(safeGetBlockEnergyManager(pair.getSecond(), pair.getFirst().getOpposite()), pair.getFirst()))
                 .filter(pair -> pair.getFirst().isPresent())
                 .forEach(pair -> {
-                    PlatformEnergyManager externalEnergy = (PlatformEnergyManager) pair.getFirst().get();
-
-                    // This weird construction is used because Botarium has duplicated PlatformEnergyManager 
-                    // that causes the "unable to cast" crash
-                    var energy = safeGetBlockEnergyManager(energyBlock, pair.getSecond()); 
-                    if (energy.isPresent())
-                        moveEnergy((PlatformEnergyManager) energy.get(), externalEnergy, 
-                                amount == -1 ? energyBlock.getEnergyStorage().getStoredEnergy() : amount);
+                    var energy = safeGetBlockEnergyManager(energyBlock, pair.getSecond());
+                    energy.ifPresent(platformEnergyManager -> moveEnergy(platformEnergyManager, pair.getFirst().get(),
+                            amount == -1 ? energyBlock.getEnergyStorage().getStoredEnergy() : amount));
                 });
     }
 
